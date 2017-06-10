e93d563181e8d63a57c827eefca43106c4dc8414
Nallely House <nallelyht@gmail.com>
2017-06-09 19:58:30 -0500

    quitando http

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var plantillaPokemons ='<div class="col s6 m3">'+'<div class="card">'+'<div class= "card-content center-align">'+'<img class="responsive-img center " src="__imagen__">'+'<span class="card-title">__nombre__</span>'+'</div>'+'</div>'+'</div>'
 
+var fotos = []
+
 $( document ).ready(function(){
 	$('.btn-floating').sideNav();
 	
-	$.getJSON("http://pokeapi.co/api/v2/pokemon/", 
+	$.getJSON("//pokeapi.co/api/v2/pokemon/", 
 						function (response) {
 		var pokemons = response.results;
 		crearPokemons(pokemons);
