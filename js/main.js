1b3d2c1d8f6b76f6ec6fd635e089b78a947d7a52
Nallely House <nallelyht@gmail.com>
2017-06-09 19:45:07 -0500

    nombres listos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-
+var plantillaPokemons ='<div class="col s6 m3">'+'<div class="card">'+'<div class= "card-content center-align">'+'<img class="responsive-img center " src="__imagen__">'+'<span class="card-title">__nombre__</span>'+'</div>'+'</div>'+'</div>'
 
 $( document ).ready(function(){
 	$('.btn-floating').sideNav();
+	
+	$.getJSON("http://pokeapi.co/api/v2/pokemon/", 
+						function (response) {
+		var pokemons = response.results;
+		crearPokemons(pokemons);
+	});
+	
+	function crearPokemons(pokemons) {
+		var plantillaFinal = "";
+		pokemons.forEach(function (pokemon) {
+			plantillaFinal += plantillaPokemons.replace("__nombre__", pokemon.name) 
+		});
+		
+		$("#pokemon-list").html(plantillaFinal);
+
+	};
+
 })
